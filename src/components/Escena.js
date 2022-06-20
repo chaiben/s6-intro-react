4ae6fbcc7a8a8f58134a7edfd879955c14b063f5
Marçal <chaiben79@gmail.com>
2022-06-20 12:09:28 +0200

    Exercise 4

--- src/components/Escena.js
+++ src/components/Escena.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 export default function Escena(props){
+  const [phraseOn, setPhraseOn] = React.useState(0);
+
+  function changeMessage(value){
+    setPhraseOn(prevPhraseOn => 
+      (prevPhraseOn + value >= phrases.length || prevPhraseOn + value < 0) ? 
+      prevPhraseOn : 
+      prevPhraseOn + value
+      )
+  }
+
+  const phrases = props.phrases.map(
+    (phrase, index) => {
+      const className = index === phraseOn ? "phrase selected" : "phrase";
+    return <p 
+      className= {className} 
+      key={`phrase${index}`} 
+      >{phrase}</p>
+    });
+
   return (
-    props.phrases.map(phrase => <p className="phrase" key={phrase}>{phrase}</p>)
+    <div>
+      <div className="menu">
+        <button onClick={() => changeMessage(-1)}>Anterior</button>
+        <button onClick={() => changeMessage(+1)}>Siguiente</button>
+      </div>
+      {phrases}
+    </div>
   );
 }
