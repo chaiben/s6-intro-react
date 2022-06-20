bc58937f40b7d57c61e7e8e4302bfd9c158cc5ee
Marçal <chaiben79@gmail.com>
2022-06-20 10:02:35 +0200

    Exercise 2

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // Components
+import { dataPhrase } from "./data";
 import Escena from "./components/Escena";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div className="container">
-    <Escena />
+    <Escena phrases={dataPhrase} />
   </div>
 );
