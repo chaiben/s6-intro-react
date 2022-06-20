c09ccbeb077ab420df2d285f39e18559217c4ffd
Marçal <chaiben79@gmail.com>
2022-06-20 09:42:20 +0200

    Sprint 6 - Nivel 1

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Components
+import Escena from "./components/Escena";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="container">
+    <Escena />
+  </div>
+);
